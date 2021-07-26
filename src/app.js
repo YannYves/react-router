f8e449533e46e3bc752034afe314a6936a16b53b
yann <yanndubois4@gmail.com>
2021-07-26 22:18:36 +0200

    feat: add router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import { BrowserRouter, Route } from "react-router-dom";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is is from a dashboard component</div>
+);
+const AddExpensePage = () => <div>This is from a AddExpenseComponent</div>;
+const EditExpensePage = () => <div>This is from a EditExpensePage</div>;
+const HelpExpensePage = () => <div>This is from a HelpExpensePage</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact={true} path="/" component={ExpenseDashboardPage} />
+      <Route exact={true} path="/create" component={AddExpensePage} />
+      <Route exact={true} path="/edit" component={EditExpensePage} />
+      <Route exact={true} path="/help" component={HelpExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
